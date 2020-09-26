0175c941479882e61e2bb8843d27986aa26d9c91
John Doolittle <jdoo.code@gmail.com>
2020-09-26 15:50:53 -0600

    Beautified getProductQuestions for readability

--- src/api/getProductQuestions.js
+++ src/api/getProductQuestions.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
-const getProductQuestions = (id) => axios.get(`http://52.26.193.201:3000/qa/${id}`)
-  .then((data) => data)
-  .catch((err) => { throw err; });
+const getProductQuestions = (id) => {
+  axios.get(`http://52.26.193.201:3000/qa/${id}`)
+    .then((data) => data)
+    .catch((err) => { throw err; });
+};
 
 export default getProductQuestions;
