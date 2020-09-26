11dd0503294c5e786775f6a86529f48ca67e369f
John Doolittle <jdoo.code@gmail.com>
2020-09-26 17:05:33 -0600

    Implemented QuestionsList boilerplate

--- src/components/QuestionsList.jsx
+++ src/components/QuestionsList.jsx
@@ -1,11 +1,26 @@
-import React, { useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import Question from './Question';
 import getProductQuestions from '../api/getProductQuestions';
 
 function QuestionsList() {
+  const [productId, setProductId] = useState(1);
+  const [questions, setQuestions] = useState([]);
+
+  useEffect(() => {
+    getProductQuestions(productId)
+      .then((res) => {
+        // console.log(res.data.results);
+        setQuestions(res.data.results);
+      })
+      .catch((err) => {
+        throw err;
+      });
+  }, [productId]);
+
   return (
     <div>
-      <Question />
+      {questions.map((question) => <Question question={question} key={question.question_id} />)}
+      <p>Load More Answers</p>
     </div>
   );
 }
