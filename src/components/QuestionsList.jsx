d0c5f4601272ee033e059aa9bf22658acf2ace95
John Doolittle <jdoo.code@gmail.com>
2020-09-26 15:46:45 -0600

    Created API folder, added getProductQuestions call

--- src/components/QuestionsList.jsx
+++ src/components/QuestionsList.jsx
@@ -1,5 +1,6 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect} from 'react';
 import Question from './Question';
+import getProductQuestions from '../api/getProductQuestions';
 
 function QuestionsList() {
   return (
@@ -10,3 +11,15 @@ function QuestionsList() {
 }
 
 export default QuestionsList;
+
+// useEffect(() => {
+//   getProductQuestionGet(userId)
+//     .then((res) => {
+//       // console.log('this is data: ', res);
+//       console.log('this is results: ', res.data.results);
+//       setProductQuestion(res.data.results[userId]);
+//     })
+//     .catch((err) => {
+//       throw err;
+//     });
+// }, []);
