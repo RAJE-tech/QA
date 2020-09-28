2f38b422d5ed0735db153fb92cc2d5cab45bb4e0
John Doolittle <jdoo.code@gmail.com>
2020-09-28 11:50:08 -0600

    Rendered AddAnswerBtn, passed down question props to btns

--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -1,12 +1,14 @@
 import React, { useState } from 'react';
 import AnswersList from './AnswersList';
 import HelpfulBtn from './HelpfulBtn';
+import AddAnswerBtn from './AddAnswerBtn';
 
 function Question({question, key}) {
   return (
     <div>
       <div>{`Q: ${question.question_body}`}</div>
-      <HelpfulBtn />
+      <HelpfulBtn question={question} />
+      <AddAnswerBtn key={key} />
       <AnswersList question={question} key={key} />
     </div>
   );
