ea357a0df61926a8f362ab77a6105f510581c861
John Doolittle <jdoo.code@gmail.com>
2020-09-28 12:02:29 -0600

    Created reportQuestion API call, adjusted markQuestionAsHelpful

--- src/api/markQuestionAsHelpful.js
+++ src/api/markQuestionAsHelpful.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const markQuestionAsHelpful = (id) => axios.post(`http://52.26.193.201:3000/qa/${id}/answers`)
+const markQuestionAsHelpful = (id) => axios.put(`http://52.26.193.201:3000/qa/${id}/helpful`)
   .then((data) => data)
   .catch((err) => { throw err; });
 
