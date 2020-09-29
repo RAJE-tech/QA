ccf1c9f252cf840e7bbfbb01fa12de525b8c4a0b
John Doolittle <jdoo.code@gmail.com>
2020-09-29 08:57:52 -0600

    Conditionally rendered seller in bold (in answerer name)

--- src/components/Answer.jsx
+++ src/components/Answer.jsx
@@ -2,10 +2,17 @@ import React, { useState } from 'react';
 import HelpfulBtn from './HelpfulBtn';
 
 function Answer({ answer }) {
+  let seller;
+  if (answer.answerer_name === 'Seller') {
+    seller = <b>{answer.answerer_name}</b>;
+  } else {
+    seller = answer.answerer_name;
+  }
   return (
     <div>
       <div>{answer.body}</div>
-      <span>{`${answer.answerer_name}, ${answer.date} | `}</span>
+      <span>{seller}</span>
+      <span>{answer.date}</span>
       <HelpfulBtn answer={answer} />
     </div>
   );
