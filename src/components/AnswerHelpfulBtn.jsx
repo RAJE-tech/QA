be28b1f88a144a63c14f7d2add475215288cfec4
John Doolittle <jdoo.code@gmail.com>
2020-09-30 09:50:40 -0600

    Implemented AnswerHelpfulBtn functionality

--- src/components/AnswerHelpfulBtn.jsx
+++ src/components/AnswerHelpfulBtn.jsx
@@ -1,11 +1,38 @@
 import React, { useState } from 'react';
+import markAnswerAsHelpful from '../api/markAnswerAsHelpful';
 
-function AnswerHelpfulBtn(props) {
+function AnswerHelpfulBtn({ answer }) {
+  const [isHelpful, setIsHelpful] = useState(false);
+  const [helpfulness, setHelpfulness] = useState(answer.helpfulness);
+
+  function markAsHelpful(event) {
+    event.preventDefault();
+    setIsHelpful(!isHelpful);
+    markAnswerAsHelpful(answer.answer_id)
+      .then((res) => {
+        setHelpfulness(helpfulness + 1);
+        console.log(res);
+      })
+      .catch((err) => {
+        throw err;
+      });
+  }
+
   return (
     <span>
       <span>Helpful? </span>
-      <span><u>Yes</u></span>
-      <span> (X) </span>
+      <span
+        onKeyDown={null}
+        onClick={!isHelpful ? markAsHelpful : null}
+        className={!isHelpful ? 'jgd-pointer' : 'jgd-pointer jgd-helpful'}
+      >
+        <u>Yes</u>
+      </span>
+      <span
+        className={!isHelpful ? null : 'jgd-helpful'}
+      >
+        {` (${helpfulness}) `}
+      </span>
     </span>
   );
 }
