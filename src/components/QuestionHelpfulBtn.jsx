5dd5ea1499b5ec9d9eaea218d8c65f1d28c277fd
John Doolittle <jdoo.code@gmail.com>
2020-09-29 18:24:07 -0600

    Implemented QuestionHelpfulBtn

--- src/components/QuestionHelpfulBtn.jsx
+++ src/components/QuestionHelpfulBtn.jsx
@@ -1,11 +1,28 @@
 import React, { useState } from 'react';
+import markQuestionAsHelpful from '../api/markQuestionAsHelpful';
 
-function QuestionHelpfulBtn(props) {
+function QuestionHelpfulBtn({ question }) {
+  const [isHelpful, setIsHelpful] = useState(false);
+  const [helpfulness, setHelpfulness] = useState(question.question_helpfulness);
+
+  function markAsHelpful(event) {
+    event.preventDefault();
+    setIsHelpful(!isHelpful);
+    markQuestionAsHelpful(question.question_id)
+      .then((res) => {
+        setHelpfulness(helpfulness + 1);
+        console.log(res);
+      })
+      .catch((err) => {
+        throw err;
+      });
+  }
+
   return (
     <span>
       <span>Helpful? </span>
-      <span><u>Yes</u></span>
-      <span> (X) </span>
+      <span onClick={!isHelpful ? markAsHelpful : null}><u>Yes</u></span>
+      <span>{` (${helpfulness}) `}</span>
     </span>
   );
 }
