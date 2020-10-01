9aa42380df117ba0a2861f8da551057fcf589a9e
John Doolittle <jdoo.code@gmail.com>
2020-10-01 17:41:51 -0600

    Gave state to FormModInputs

--- src/components/FormModInputs.jsx
+++ src/components/FormModInputs.jsx
@@ -3,7 +3,16 @@ import axios from 'axios';
 import { Form, Button } from 'react-bootstrap';
 
 const FormModInputs = ({ onHide, question }) => {
+  const [state, setState] = useState({
+    body: '',
+    name: '',
+    email: '',
+    photos: [],
+  });
+  }
+
   return (
+
     <div>
     </div>
   );
