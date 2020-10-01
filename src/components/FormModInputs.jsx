3977cb8fc94f510bb5a66f684c8448587f9ec7ef
John Doolittle <jdoo.code@gmail.com>
2020-10-01 17:42:25 -0600

    Created submitForm handler

--- src/components/FormModInputs.jsx
+++ src/components/FormModInputs.jsx
@@ -9,6 +9,26 @@ const FormModInputs = ({ onHide, question }) => {
     email: '',
     photos: [],
   });
+
+  function submitForm(event) {
+    event.preventDefault();
+    axios({
+      method: 'post',
+      url: `http://52.26.193.201:3000/qa/${question.question_id}/answers`,
+      data: {
+        body: state.body,
+        name: state.name,
+        email: state.email,
+        photos: state.photos,
+      },
+    })
+      .then(() => {
+        onHide();
+      })
+      .catch((error) => {
+        throw error;
+      });
+  }
   }
 
   return (
