eade347eb3849d718c49d4b6e761aad6912243c5
John Doolittle <jdoo.code@gmail.com>
2020-10-01 14:01:13 -0600

    Gave state to AnswerBtn, imported FormModal

--- src/components/AddAnswerBtn.jsx
+++ src/components/AddAnswerBtn.jsx
@@ -1,8 +1,15 @@
 import React, { useState } from 'react';
+import FormModal from './FormModal';
 
 function AddAnswerBtn() {
+  const [show, setShow] = useState(false);
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
   return (
-    <span className="jgd-small-text jgd-faded"><u>Add Answer</u></span>
+    <span>
+      <span className="jgd-small-text jgd-pointer jgd-faded" onClick={handleShow} onKeyDown={null}><u>Add Answer</u></span>
+      <FormModal show={show} onHide={handleClose} />
+    </span>
   );
 }
 
