ae9eb42a84b5bf26a6bfd4b6b0f73906a7c06095
John Doolittle <jdoo.code@gmail.com>
2020-10-03 11:10:29 -0600

    Installed bootstrap-icons, styled search border and placeholder text

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 
 function Search() {
   return (
-    <input type="search" className="form-control ds-input" id="search-input" placeholder="HAVE A QUESTION? SEARCH FOR ANSWERS..." aria-label="Search for..." autoComplete="off" spellCheck="false" role="searchbox" aria-autocomplete="list" aria-owns="algolia-autocomplete-listbox-0" />
+    <input type="search" className="form-control ds-input jgd-small-bold jgd-sharp-edge jgd-large-padded jgd-solid-border" id="search-input" placeholder="HAVE A QUESTION? SEARCH FOR ANSWERS..." aria-label="Search for..." autoComplete="off" spellCheck="false" role="searchbox" aria-autocomplete="list" aria-owns="algolia-autocomplete-listbox-0" />
   );
 }
 
