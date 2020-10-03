44a0ada219503b90ea43639a017f70fb02dc3c3e
John Doolittle <jdoo.code@gmail.com>
2020-10-03 09:29:17 -0600

    Setup QuestionFormModal, created QModalInputs file

---
+++ src/components/QuestionFormModal.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import { Modal } from 'react-bootstrap';
+import QuestionFormModInputs from './QuestionFormModInputs';
+
+function QuestionFormModal(productName) {
+  if (show) {
+    return (
+      <Modal show={show} onHide={onHide}>
+        <Modal.Dialog>
+          <Modal.Header closeButton>
+            <Modal.Title>Ask Your Question</Modal.Title>
+          </Modal.Header>
+
+          <Modal.Body>
+            <p>{`About the ${productName}`}</p>
+            <QuestionFormModInputs
+              onHide={onHide}
+              question={question}
+              setQuestions={setQuestions}
+            />
+          </Modal.Body>
+        </Modal.Dialog>
+      </Modal>
+    );
+  }
+  return <div />;
+}
+
+export default QuestionFormModal;
