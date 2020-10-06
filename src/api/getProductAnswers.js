81975855377b5dcd72e43280238f4a7f55064173
John Doolittle <jdoo.code@gmail.com>
2020-10-05 19:24:40 -0600

    Adjusted getProductQuestions and getProductAnswers calls to yield more result

--- src/api/getProductAnswers.js
+++ src/api/getProductAnswers.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const getProductAnswers = (id) => axios.get(`http://52.26.193.201:3000/qa/${id}/answers`)
+const getProductAnswers = (id) => axios.get(`http://52.26.193.201:3000/qa/${id}/answers?count=50`)
   .then((data) => data)
   .catch((err) => { throw err; });
 
--- src/api/getProductQuestions.js
+++ src/api/getProductQuestions.js
@@ -1,10 +1,6 @@
 import axios from 'axios';
 
-const req = {
-  count: 200,
-};
-
-const getProductQuestions = (id) => axios.get(`http://52.26.193.201:3000/qa/${id}?count=50`, req)
+const getProductQuestions = (id) => axios.get(`http://52.26.193.201:3000/qa/${id}?count=50`)
   .then((data) => data)
   .catch((err) => { throw err; });
 
