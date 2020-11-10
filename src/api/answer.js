f3ce02a4cfdbd7748d05c27679f3f94e51ebac6c
John Doolittle <jdoo.code@gmail.com>
2020-11-09 20:27:08 -0700

    Converted addAnswer to Question api

--- src/api/answer.js
+++ src/api/answer.js
@@ -1,9 +1,7 @@
 import axios from 'axios';
 
 const Answer = {
-  addAnswer: (id) => axios.post(`http://52.26.193.201:3000/qa/${id}/answers`)
-  .then((data) => data)
-  .catch((err) => { throw err; }),
+
 };
 
 export default Answer;
--- src/api/question.js
+++ src/api/question.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
 const Question = {
-
+  addAnswer: (id) => axios.post(`http://52.26.193.201:3000/qa/${id}/answers`)
+    .then((data) => data)
+    .catch((err) => { throw err; }),
 };
 
 export default Question;
