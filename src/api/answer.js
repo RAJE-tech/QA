69262acc74bfde9004f2d0ff2061fbb65b081ee8
John Doolittle <jdoo.code@gmail.com>
2020-11-09 20:36:17 -0700

    Bundled reportAnswer api

--- src/api/answer.js
+++ src/api/answer.js
@@ -4,6 +4,9 @@ const Answer = {
   markAsHelpful: (id) => axios.put(`http://52.26.193.201:3000/qa/answer/${id}/helpful`)
     .then((data) => data)
     .catch((err) => { throw err; }),
+  report: (id) => axios.put(`http://52.26.193.201:3000/qa/answer/${id}/report`)
+    .then((data) => data)
+    .catch((err) => { throw err; }),
 };
 
 export default Answer;
