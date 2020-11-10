d6b8ce466558eeed6cdf702065963d842f9669e4
John Doolittle <jdoo.code@gmail.com>
2020-11-09 21:02:08 -0700

    Removed cruft useState import in Answer.jsx

--- src/components/Answer.jsx
+++ src/components/Answer.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React from 'react';
 import AnswerHelpfulBtn from './AnswerHelpfulBtn';
 import ReportAnswerBtn from './ReportAnswerBtn';
 import AnswerImages from './AnswerImages';
