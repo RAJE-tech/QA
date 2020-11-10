2803ea6f3a8ced8e91a70a537ca9f585889968a8
John Doolittle <jdoo.code@gmail.com>
2020-11-09 20:32:05 -0700

    Bundled markQuestionAsHelpful api

--- src/api/question.js
+++ src/api/question.js
@@ -7,6 +7,9 @@ const Question = {
   getAnswers: (id) => axios.get(`http://52.26.193.201:3000/qa/${id}/answers?count=50`)
     .then((data) => data)
     .catch((err) => { throw err; }),
+  markAsHelpful: (id) => axios.put(`http://52.26.193.201:3000/qa/question/${id}/helpful`)
+    .then((data) => data)
+    .catch((err) => { throw err; }),
 };
 
 export default Question;
