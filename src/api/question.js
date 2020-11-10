f9be59b5ef5971fda13ee0163779f1374889d515
John Doolittle <jdoo.code@gmail.com>
2020-11-09 20:33:16 -0700

    Bundled reportQuestion api

--- src/api/question.js
+++ src/api/question.js
@@ -10,6 +10,9 @@ const Question = {
   markAsHelpful: (id) => axios.put(`http://52.26.193.201:3000/qa/question/${id}/helpful`)
     .then((data) => data)
     .catch((err) => { throw err; }),
+  report: (id) => axios.put(`http://52.26.193.201:3000/qa/question/${id}/report`)
+    .then((data) => data)
+    .catch((err) => { throw err; }),
 };
 
 export default Question;
--- src/api/reportQuestion.js
+++
@@ -1,7 +0,0 @@
-import axios from 'axios';
-
-const reportQuestion = (id) => axios.put(`http://52.26.193.201:3000/qa/question/${id}/report`)
-  .then((data) => data)
-  .catch((err) => { throw err; });
-
-export default reportQuestion;
