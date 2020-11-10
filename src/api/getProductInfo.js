67bbcdde8f494e8ae24604ae536241890ff4448b
John Doolittle <jdoo.code@gmail.com>
2020-11-09 20:39:25 -0700

    Bundled getProductInfo api

--- src/api/getProductInfo.js
+++
@@ -1,7 +0,0 @@
-import axios from 'axios';
-
-const getProductInfo = (id) => axios.get(`http://52.26.193.201:3000/products/${id}`)
-  .then((data) => data)
-  .catch((err) => { throw err; });
-
-export default getProductInfo;
--- src/api/product.js
+++ src/api/product.js
@@ -3,7 +3,10 @@ import axios from 'axios';
 const Product = {
   getQuestions: (id) => axios.get(`http://52.26.193.201:3000/qa/${id}?count=50`)
     .then((data) => data)
-    .catch((err) => { throw err; });
+    .catch((err) => { throw err; }),
+  getProduct: (id) => axios.get(`http://52.26.193.201:3000/products/${id}`)
+    .then((data) => data)
+    .catch((err) => { throw err; }),
 };
 
 export default Product;
