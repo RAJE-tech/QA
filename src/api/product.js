a63b9fee2b6086f0c0aad78bbd58ed1a6daa7ed2
John Doolittle <jdoo.code@gmail.com>
2020-11-09 20:37:21 -0700

    Bundled getProductQuestions api

--- src/api/product.js
+++ src/api/product.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
 const Product = {
-
+  getQuestions: (id) => axios.get(`http://52.26.193.201:3000/qa/${id}?count=50`)
+    .then((data) => data)
+    .catch((err) => { throw err; });
 };
 
 export default Product;
