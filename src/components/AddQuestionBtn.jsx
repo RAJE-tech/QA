0d0537efabfb7119c3cf92a2355c6b82c35823c1
John Doolittle <jdoo.code@gmail.com>
2020-11-18 16:59:55 -0700

    Removed cruft code from AddQuestionBtn

--- src/components/AddQuestionBtn.jsx
+++ src/components/AddQuestionBtn.jsx
@@ -8,16 +8,11 @@ function AddQuestionBtn({ productId, productName, setQuestions }) {
   const handleShow = () => setShow(true);
   return (
     <div className="col">
-      <Button variant="outline-dark jgd-sharp-edge jgd-large-padded jgd-white-background" size="lg" onClick={handleShow}>
-        <span className="jgd-small-bold">
-          ADD A QUESTION
-        </span>
+      <Button variant="outline-dark jgd-sharp-edge jgd-large-padded jgd-white-background jgd-small-bold" size="lg" onClick={handleShow}>
+        ADD A QUESTION
         <span className="jgd-divider" />
-        <span className="jgd-small-bold">
-          +
-        </span>
+        +
       </Button>
-      {' '}
       <QuestionFormModal
         show={show}
         productId={productId}
